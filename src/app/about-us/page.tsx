de44d865b8c035626ea75f19575da7fdbcc3288b
prarathod <itsmeprajwal2@gmail.com>
2024-10-05 16:45:52 +0530

    test03

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function AboutUs() {
-  return (
-    <div>AboutUs</div>
-  )
+  return <div>AboutUs</div>;
 }
 
-export default AboutUs
+export default AboutUs;
