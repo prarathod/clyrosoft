fe0a2e3fde07a3afe0e943cac65eb9086e1a3699
prarathod <itsmeprajwal2@gmail.com>
2024-10-05 18:14:51 +0530

    toggle added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { ThemeModeToggle } from '@/components/common/toggleButton/ThemeModeToggle';
 
 /**
  * This is the home page
@@ -8,6 +9,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <ThemeModeToggle />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
