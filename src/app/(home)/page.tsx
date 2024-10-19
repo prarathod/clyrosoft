8f607b6bf5005f898c255d5b77e5440546b0c6a7
prarathod <itsmeprajwal2@gmail.com>
2024-10-19 17:36:09 +0530

    layout issue fixed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import HeroSection from "../../components/home/HeroSection";
-import Footer from "./Footer";
+
 import Navbar from "../../components/common/navbar/Navbar";
+import Footer from "@/components/common/footer/Footer";
 
 export default function HomeApp() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow bg-gray-100">
-        <HeroSection />
-      </main>
-      <Footer />
-    </div>
+    <main className="flex-grow bg-gray-100">
+      <HeroSection />
+    </main>
   );
 }
