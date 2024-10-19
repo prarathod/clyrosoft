925b3db8856344a74cec7a373fc8312d178cfb46
prarathod <itsmeprajwal2@gmail.com>
2024-10-19 18:03:26 +0530

    remove unwanted code

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import HeroSection from "../../components/home/HeroSection";
 
-import Navbar from "../../components/common/navbar/Navbar";
-import Footer from "@/components/common/footer/Footer";
-
 export default function HomeApp() {
   return (
     <main className="flex-grow bg-gray-100">
